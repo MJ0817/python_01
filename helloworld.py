a971489e0b2b49d98f03b3df1eeafe47c473109e
MJ0817 <minjoonyy@naver.com>
2024-06-26 17:02:36 +0900

    파이썬 기본 입출력 및 사칙연산
    1. 파이썬 설치 및 파이참 설치
    2. 파이썬 기본 입출력
    3. 파이썬 기본 사칙연산 및 논리 연산

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+
+name = "김민준"
+print(name)
+name = input("이름을 입력하세요 : ")
+print("안녕하세요, + name님")
+print("안녕하세요, " + name + "님. 반갑습니다.")
+name = input("이름을 입력하세요 :")
+age = input("나이를 입력하세요 :")
+print(f"{name}은 {age}살 입니다.")
---
+++ 사칙연산.py
@@ -0,0 +1,15 @@
+print(10 ** 5)
+a = 100
+b = 5
+
+print(a / b)
+
+#비교 연산자
+a = 10
+b = 5
+
+c = 8
+
+print(a >= b)
+print(a == b)
+print(a != b)
